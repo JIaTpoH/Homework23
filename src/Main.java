cd33dbfa7c76e48a88351c19ca1d2f99780c11db
Dmitri Mandaji <badmopss@gmail.com>
2023-03-05 15:59:18 +0100

    Error File

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-  public static void main(String[] args) {
+  public static <String> void main(String[] args) {
+    java.lang.Object System;
     System.out.println("Hello world!");
   }
 }
